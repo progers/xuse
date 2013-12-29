2ac8d7a4fd6b3acf7621632c21661d206176023d
Philip Rogers <pdr@chromium.org>
2013-12-28 19:40:55 -0500

    Get basic target element invalidation working

--- xuse.js
+++ xuse.js
@@ -1,14 +1,39 @@
 var XUsePrototype = Object.create(SVGGraphicsElement.prototype);
 
-XUsePrototype._updateHrefAttribute = function(forceReclone) {
+XUsePrototype._updateHrefAttribute = function(reclone) {
   var href = this.getAttributeNS('http://www.w3.org/1999/xlink', 'href');
-  if (!href)
-    return;
   var hrefElement = document.querySelector(href);
-  if (forceReclone || this._targetElement != hrefElement) {
+
+  if (this._targetElement != hrefElement) {
+    // Clean up the old shadow tree and mutation observer.
+    if (this._targetMutationObserver)
+      this._targetMutationObserver.disconnect();
+    if (this._targetElement)
+      this.shadowRoot.innerHTML = '';
+
+    // Update our target.
     this._targetElement = hrefElement;
-    this.shadowRoot.appendChild(hrefElement.cloneNode(true));
+    reclone = true;
+
+    // Hook up a shiny new observer to listen for target modifications.
+    if (this._targetElement) {
+      this._targetMutationObserver = new MutationObserver(function(mutations) {
+        this._updateHrefAttribute(true);
+      }.bind(this));
+      this._targetMutationObserver.observe(this._targetElement, {
+        attributes: true,
+        childList: true,
+        characterData: true
+      });
+    }
   }
+
+  if (reclone && this._targetElement)
+    this.shadowRoot.appendChild(this._targetElement.cloneNode(true));
+
+  // FIXME: Need to add a global mutation observer to listen for cases when document.querySelector(#href)
+  //        changes. For example, if a new element is added to the document before the old target and
+  //        with the same id, we need to update the target to refer to the first element that matches #href.
 };
 
 XUsePrototype._updateXAttribute = function() {
@@ -51,8 +76,7 @@ XUsePrototype.createdCallback = function() {
 XUsePrototype.attributeChangedCallback = function(attributeName, oldValue, newValue) {
   switch (attributeName) {
     // FIXME: attributeChangedCallback doesn't support namespaces so we have to check for both xlink:href and href.
-    case('xlink:href'):
-    case('href'):
+    case('xlink:href'): case('href'):
       this._updateHrefAttribute();
       break;
     case('x'):
